753712119f5e074eebf539e79dba240f4da1987a
CarlosAl21 <calvarado2731@uta.edu.ec>
2022-12-05 09:47:06 -0500

    cos

--- src/Gestion/Tipo.java
+++ src/Gestion/Tipo.java
@@ -151,12 +151,19 @@ public class Tipo {
         return false;
     }
     
-    public void Imprimir(){   
+    public void ImprimirTodo(){   
         for(int i=0;i<this.lista.size();i++){
             for(int j=0;j<this.lista.get(i).size();j++){
                 System.out.println(this.lista.get(i).get(j).toString());
                 
             }
+        }
+    }
+
+    public void ImprimirLista(ArrayList<Auto>b){ 
+        int pos=this.lista.indexOf(b);  
+        for(int i=0;i<b.size();i++){
+            System.out.println(this.lista.get(pos).get(i).toString());
         }
     }
 
