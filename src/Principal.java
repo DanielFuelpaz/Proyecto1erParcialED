e4c2bdda3341f91ab3f8910da9c18e8e73d4fbf1
Daniel Fuelpaz <efuelpaz8870@uta.edu.ec>
2022-12-06 11:46:55 -0500

    Cambios

--- src/Principal.java
+++ src/Principal.java
@@ -2,21 +2,36 @@ import Gestion.GestorClientes;
 import Clases.Cliente;
 import java.time.LocalDate;
 import java.time.Month;
-
-import javax.security.sasl.AuthorizeCallback;
+import java.util.Scanner;
 
 public class Principal {
         public static void main(String[] args) {
+                String respuesta;
                 GestorClientes GC = new GestorClientes();
+                System.out.println("Bienvenido al Sistema de Clientes");
+                System.out.println("Desea ingresar un cliente");
+                Scanner leer = new Scanner(System.in);
+                respuesta = leer.nextLine();
+                if (respuesta.equalsIgnoreCase("si")) {
+                        Cliente c1 = new Cliente();
+                        System.out.println("Ingrese su # de cedula:");
+                        c1.cedula = leer.nextLine();
+                        System.out.println("Ingrese su nombre:");
+                        System.out.println("Ingrese su Apellido:");
+                        System.out.println("Ingrese su # de telefono:");
+                        System.out.println("Ingrese su dirección:");
+
+                } else {
+                }
                 LocalDate fecha = LocalDate.now();
                 LocalDate fecha2 = LocalDate.of(2022, Month.DECEMBER, 7);
+                LocalDate fecha3 = LocalDate.now().plusDays(10);
                 Cliente cliente1 = new Cliente("1850178870", "Stalin", "Badillo", "0912345678", "Mall de los Andes",
                                 true,
                                 fecha);
                 Cliente cliente2 = new Cliente("1850178856", "Carlos", "Alvarado", "0919341478",
                                 "Parque 12 de Noviembre", true,
                                 fecha2.plusDays(43));
-                LocalDate fecha3 = LocalDate.now().plusDays(10);
                 Cliente cliente3 = new Cliente("1805744786", "Karla", "Torres", "0943509543", "Paseo shopping Ambato",
                                 true,
                                 fecha3);
