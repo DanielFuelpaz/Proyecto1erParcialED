23dfe8620d8f04bcbac96792b262ad45ba5af6b3
CarlosAl21 <calvarado2731@uta.edu.ec>
2022-12-05 19:43:40 -0500

    siiii

--- src/MainFactura.java
+++ src/MainFactura.java
@@ -9,11 +9,13 @@ public class MainFactura {
         ArrayList<Auto> interes= new ArrayList();
         GestionCompra coso= new GestionCompra(interes);
         Auto Audi =new Auto("das5465", "Azul", "Audi", "2018");
-        Auto Mercedes =new Auto("das5465", "Azul", "Audi", "2018");
-        Auto Mclaren =new Auto("das5465", "Azul", "Audi", "2018");
+        Auto Mercedes =new Auto("csi1245", "Azul", "Mercedes", "2015");
+        Auto Mclaren =new Auto("ghf1346", "Azul", "Maclaren", "2013");
         coso.Agregar(Audi);
         coso.Agregar(Mclaren);
         coso.Agregar(Mercedes);
+        coso.MostrarLista();
+        coso.BorrarCarro(2);
         coso.GenerarFactura("21/12/2022", 15000.45);
         coso.ImprimirFactura();
     }
