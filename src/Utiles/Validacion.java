084f83f4eeb3e1c4d1669d5640ba68d0ee75348e
GabooMedina <gmedina3108@uta.edu.ec>
2022-12-08 11:56:00 -0500

    Control  Opcion

--- src/Utiles/Validacion.java
+++ src/Utiles/Validacion.java
@@ -4,10 +4,16 @@ public class Validacion {
     public boolean validarCedula(String cedula) {
         return cedula.matches("[0-9]{10}");
     }
+
     public boolean validarTelefono(String telefono) {
         return telefono.matches("09[0-9]{8}");
     }
-        public boolean validarPersona(String telefono) {
+
+    public boolean validarPersona(String telefono) {
         return telefono.matches("[A-Z]([a-z])*");
+    }
+
+    public boolean validarOpcion(String opcion) {
+        return opcion.matches("[1-7]");
     }
 }
